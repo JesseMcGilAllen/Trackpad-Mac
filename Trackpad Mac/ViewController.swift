1a4457f3e6a9a2b1928a2aff205170005ce87af8
Jesse McGil Allen <jmallen7@gmail.com>
2015-02-14 23:19:37 -0600

    Initial Commit

---
+++ Trackpad Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Trackpad Mac
+//
+//  Created by Jesse McGil Allen on 2/14/15.
+//  Copyright (c) 2015 Jesse McGil Allen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
