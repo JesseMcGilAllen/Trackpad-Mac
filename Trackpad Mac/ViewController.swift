195c8027931ccafd5ed007fab3d7c05f743606a8
Jesse McGil Allen <jmallen7@gmail.com>
2015-02-16 15:53:16 -0600

    pairing successful

--- Trackpad Mac/ViewController.swift
+++ Trackpad Mac/ViewController.swift
@@ -9,9 +9,11 @@
 import Cocoa
 import CoreBluetooth
 
-class ViewController: NSViewController, CBCentralManagerDelegate {
+class ViewController: NSViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
     
     let trackpadServiceUUID = CBUUID(string: "AB8A3096-046C-49DD-8709-0361EC31EFED")
+    let trackingCharacteristicUUID = CBUUID(string: "7754BF4E-9BB5-4719-9604-EE48A565F09C")
+    
     var centralManager : CBCentralManager!
     var discoveredPeripheral : CBPeripheral!
     
@@ -58,6 +60,11 @@ class ViewController: NSViewController, CBCentralManagerDelegate {
     
     func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
         println("Connected!")
+        
+        peripheral.delegate = self
+        
+        peripheral.discoverServices([trackpadServiceUUID])
+        
     }
     
     func centralManager(central: CBCentralManager!, didFailToConnectPeripheral peripheral: CBPeripheral!, error: NSError!) {
@@ -67,6 +74,41 @@ class ViewController: NSViewController, CBCentralManagerDelegate {
             println("Error publishing service: \(error.localizedDescription)")
         }
     }
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverServices error: NSError!) {
+        
+        if error != nil {
+            println("Error discovering service: \(error.localizedDescription)")
+        }
+        
+        for service in peripheral.services {
+            peripheral.discoverCharacteristics([trackingCharacteristicUUID], forService: service as! CBService)
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverCharacteristicsForService service: CBService!, error: NSError!) {
+        
+        if error != nil {
+            println("Error discovering characteristic: \(error.localizedDescription)")
+        }
+        
+        for characteristic in service.characteristics {
+            let characteristicUUID = characteristic.UUID
+            println(characteristic.UUIDString)
+            println(trackingCharacteristicUUID.UUIDString)
+            
+            
+            
+            if characteristicUUID == trackingCharacteristicUUID {
+                peripheral.setNotifyValue(true, forCharacteristic: characteristic as! CBCharacteristic)
+                println("Success!")
+            }
+            
+            
+        }
+    }
+    
+
 
 
 }
