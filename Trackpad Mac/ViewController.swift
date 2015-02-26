5a5693fdb2ea6c54913e03196080b426a677ddb7
Jesse McGil Allen <jmallen7@gmail.com>
2015-02-25 19:39:47 -0600

    adding scaling factor to cursor movement

--- Trackpad Mac/ViewController.swift
+++ Trackpad Mac/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: NSViewController, CBCentralManagerDelegate, CBPeripheralDe
     
     var centralManager : CBCentralManager!
     var discoveredPeripheral : CBPeripheral?
+    var scalingFactor : CGPoint!
     
     required init?(coder: NSCoder) {
         
@@ -39,6 +40,8 @@ class ViewController: NSViewController, CBCentralManagerDelegate, CBPeripheralDe
         }
     }
     
+    // MARK: UUIDs
+
     func trackpadServiceUUID() -> CBUUID {
         
         return CBUUID(string: "AB8A3096-046C-49DD-8709-0361EC31EFED")
@@ -47,6 +50,11 @@ class ViewController: NSViewController, CBCentralManagerDelegate, CBPeripheralDe
     func trackingCharacteristicUUID() -> CBUUID {
         
         return CBUUID(string: "7754BF4E-9BB5-4719-9604-EE48A565F09C")
+    }
+    
+    func screenSizeCharacteristicUUID() -> CBUUID {
+        
+        return CBUUID(string: "92241F88-3A7E-4DEA-8DE5-12066D690250")
     }
     
     // MARK: Discovering Peripherals
@@ -105,19 +113,27 @@ class ViewController: NSViewController, CBCentralManagerDelegate, CBPeripheralDe
         }
         
         for characteristic in service.characteristics {
-            let characteristicUUID = characteristic.UUID
-            println(characteristicUUID)
-            println(trackingCharacteristicUUID().UUIDString)
             
-            if characteristicUUID == trackingCharacteristicUUID() {
-               peripheral.setNotifyValue(true, forCharacteristic: characteristic as! CBCharacteristic)
-               
-               println("Success!")
-            }
-            
+            discoverCharacteristic(characteristic as! CBCharacteristic, forPeripheral: peripheral)
         }
     }
-
+    
+    func discoverCharacteristic(characteristic : CBCharacteristic, forPeripheral peripheral: CBPeripheral) {
+        
+        
+        
+        if characteristic.UUID == trackingCharacteristicUUID() {
+            peripheral.setNotifyValue(true, forCharacteristic: characteristic as CBCharacteristic)
+            
+            
+        } else if characteristic.UUID == screenSizeCharacteristicUUID() {
+            
+            peripheral.readValueForCharacteristic(characteristic)
+        }
+        
+        
+    }
+    
     func peripheral(peripheral: CBPeripheral!, didUpdateNotificationStateForCharacteristic characteristic: CBCharacteristic!, error: NSError!) {
 
         if error != nil {
@@ -141,29 +157,73 @@ class ViewController: NSViewController, CBCentralManagerDelegate, CBPeripheralDe
         
         let data = characteristic.value()
         
-        movingCursor(pointFromData(data))
+        if characteristic.UUID == trackingCharacteristicUUID() {
+            
+            movingCursor(pointFromData(data))
+            
+        } else if characteristic.UUID == screenSizeCharacteristicUUID() {
+            
+            calculateScalingFactor(nsRectFromData(data))
+        }
+        
+        
 
     }
     
     // MARK: moving cursor
     
     func movingCursor(location : NSPoint) {
+        
         if location != NSPoint(x: -1.0, y: 0.0) {
             CGDisplayMoveCursorToPoint(CGMainDisplayID(), location)
         }
     }
     
     func pointFromData(data : NSData) -> NSPoint {
+        
         let dataString = NSString(data: data, encoding: NSUTF8StringEncoding)
         
         if dataString != nil {
-            return NSPointFromString(dataString as! String)
+            
+            let point = NSPointFromString(dataString as! String)
+            return pointMultiplyScalar(point, scalar: scalingFactor)
+            
         } else {
+            
             return NSPoint(x: -1.0, y: 0)
         }
     }
     
+    func pointMultiplyScalar(point : CGPoint, scalar : CGPoint) -> CGPoint {
+        
+        return CGPoint(x: point.x * scalar.x, y: point.y * scalar.y)
+    }
     
+    func calculateScalingFactor(screenSize : NSRect) {
+        
+        let monitor = NSScreen.mainScreen()?.frame
+        
+        let xScale = monitor!.width / screenSize.width
+        let yScale = monitor!.height / screenSize.height
+        
+        scalingFactor = CGPoint(x: xScale, y: yScale)
+        
+        
+    }
+    
+    func nsRectFromData(data : NSData) -> NSRect {
+        let dataString = NSString(data: data, encoding: NSUTF8StringEncoding)
+        
+        if dataString != nil {
+            
+            return NSRectFromString(dataString as! String)
+            
+        } else {
+            
+            return NSRect( x: 0, y: 0, width: 0, height: 0)
+        }
+        
+    }
 }
 
 
