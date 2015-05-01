9dfc1e0bfdd6eef821f5c9acab3bb6b6566b7dfa
Jesse McGil Allen <jmallen7@gmail.com>
2015-05-01 14:04:33 -0500

    adding Objective-C class to handle scroll wheel events

---
+++ Trackpad Mac/ScrollWheelHandler.h
@@ -0,0 +1,13 @@
+//
+//  ScrollWheelHandler.h
+//  Trackpad Mac
+//
+//  Created by Jesse McGil Allen on 5/1/15.
+//  Copyright (c) 2015 Jesse McGil Allen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScrollWheelHandler : NSObject
+
+@end
---
+++ Trackpad Mac/ScrollWheelHandler.m
@@ -0,0 +1,13 @@
+//
+//  ScrollWheelHandler.m
+//  Trackpad Mac
+//
+//  Created by Jesse McGil Allen on 5/1/15.
+//  Copyright (c) 2015 Jesse McGil Allen. All rights reserved.
+//
+
+#import "ScrollWheelHandler.h"
+
+@implementation ScrollWheelHandler
+
+@end
